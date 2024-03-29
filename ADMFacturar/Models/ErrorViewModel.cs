44281c602a45b791c818f2f2c6227903cafbff6c
JosethAG <93845320+JosethAG@users.noreply.github.com>
2024-03-29 16:07:21 -0600

    Add project files.

---
+++ ADMFacturar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADMFacturar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
