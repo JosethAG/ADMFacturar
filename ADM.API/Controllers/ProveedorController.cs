ac884279ea599c2eac049c3e8af99f1a66567c52
ADelgado-99 <74467915+ADelgado-99@users.noreply.github.com>
2024-06-09 18:48:20 -0600

    Implementacion Proveedor
    Listar, modificar, y eliminar proveedor con API

---
+++ ADM.API/Controllers/ProveedorController.cs
@@ -0,0 +1,88 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using ADM.API;
+using ADM.Models;
+using ADM.Service;
+using Microsoft.AspNetCore.Mvc;
+using ADM.Interface;
+using ADM.Architectur;
+using ADM.Architecture;
+using Newtonsoft.Json;
+
+
+namespace ADM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProveedorController : Controller
+    {
+        private IProveedorService? _ProveedorService;
+
+        [HttpGet]
+        [Route("Listar")]
+        public IEnumerable<Proveedor> ListaProveedores(string? Nombre)
+        {
+            DataTable tProveedor = null;
+
+            if (Nombre == null)
+            {
+                tProveedor = DBData.List("sp_ListarProveedores");
+            }
+            else
+            {
+                List<DBParameter> parameters = new List<DBParameter>
+                    {
+                        new DBParameter("@Nombre", Nombre)
+                    };
+                tProveedor = DBData.List("sp_ListarProveedoresxNombre", parameters);
+            }
+            string jsonArticle = JsonConvert.SerializeObject(tProveedor);
+            var result = JsonProvider.DeserializeSimple<IEnumerable<Proveedor>>(jsonArticle);
+            return result;
+        }
+
+        [HttpPost]
+        [Route("Crear")]
+        public bool CrearProveedor(Proveedor Proveedor)
+        {
+            List<DBParameter> parameters = new List<DBParameter>
+            {
+                new DBParameter("@P_PK_Proveedor", Proveedor.PK_Proveedor.ToString()),
+                new DBParameter("@P_Nombre", Proveedor.Nombre),
+                new DBParameter("@P_Telefono", Proveedor.Telefono),
+                new DBParameter("@P_Correo", Proveedor.Correo),
+                new DBParameter("@@P_Direccion", Proveedor.Direccion),
+                new DBParameter("@P_Estado", Proveedor.Estado.ToString()),
+                new DBParameter("@P_FK_Usuario_Creacion", Proveedor.FK_Usuario_Creacion),
+                new DBParameter("@P_FK_Usuario_Modificacion", Proveedor.FK_Usuario_Modificacion),
+                new DBParameter("@P_Fecha_Creacion", Proveedor.FechaCreacion.ToString()),
+                new DBParameter("@P_Fecha_Modificacion", Proveedor.FechaModificacion.ToString())
+            };
+
+            var result = DBData.Execute("sp_InsertarModificarProveedors", parameters);
+
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public bool EliminarProveedor(string Id)
+        {
+            if (Id == null || Id.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                List<DBParameter> parameters = new List<DBParameter>
+                {
+                    new DBParameter("@P_PK_Proveedor", Id)
+                };
+                
+                var result = DBData.Execute("sp_EliminarProveedores", parameters);
+
+                return !result;
+            }
+        }
+    }
+}
---
+++ ADM.Interface/IProveedorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADM.Models;
+
+namespace ADM.Interface
+{
+    public interface IProveedorService
+    {
+        IEnumerable<Proveedor> ListaProveedors(string? Nombre);
+        bool CrearUpdProveedor(Proveedor Proveedor);
+        bool EliminarProveedor(string Id);
+
+    }
+}
