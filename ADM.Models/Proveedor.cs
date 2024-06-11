d3aa93bba8a87d32a9ed70888f421c5609fdef7e
ADelgado-99 <74467915+ADelgado-99@users.noreply.github.com>
2024-06-10 22:58:19 -0600

    Correciones generales

--- ADM.Models/Proveedor.cs
+++ ADM.Models/Proveedor.cs
@@ -9,8 +9,8 @@
         public string Direccion { get; set; }
         public bool Estado { get; set; }
         public string FK_Usuario_Creacion { get; set; }
-        public string FK_Usuario_Modificacion { get; set; }
-        public DateTime FechaCreacion { get; set; }
-        public DateTime FechaModificacion { get; set; }
+        public string? FK_Usuario_Modificacion { get; set; }
+        public DateTime Fecha_Creacion { get; set; }
+        public DateTime? Fecha_Modificacion { get; set; }
     }
 }
--- ADM.Models/Vendedor.cs
+++ ADM.Models/Vendedor.cs
@@ -8,8 +8,8 @@
         public string Correo { get; set; }
         public bool Estado { get; set; }
         public string FK_Usuario_Creacion { get; set; }
-        public string FK_Usuario_Modificacion { get; set; }
-        public DateTime FechaCreacion { get; set; }
-        public DateTime FechaModificacion { get; set; }
+        public string? FK_Usuario_Modificacion { get; set; }
+        public DateTime Fecha_Creacion { get; set; }
+        public DateTime? Fecha_Modificacion { get; set; }
     }
 }
