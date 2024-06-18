87bf371f1c01073b58b827d4e256f951cfddd8a4
ADelgado-99 <74467915+ADelgado-99@users.noreply.github.com>
2024-06-18 12:21:59 -0600

    Update Script BD ADM.sql

--- Script BD ADM.sql
+++ Script BD ADM.sql
@@ -366,8 +366,7 @@ BEGIN
 END;
 GO
 
-
-/****** Object:  StoredProcedure [dbo].[sp_ModificarArticulos]    Script Date: 6/11/2024 8:56:30 AM ******/
+/****** Object:  StoredProcedure [dbo].[sp_ModificarArticulos]    Script Date: 6/18/2024 12:10:07 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -382,9 +381,7 @@ CREATE PROCEDURE [dbo].[sp_ModificarArticulos]
     @P_Costo DECIMAL(18, 2),
     @P_Precio DECIMAL(18, 2),
     @P_Estado BIT,
-    @P_FK_Usuario_Creacion VARCHAR(100),
     @P_FK_Usuario_Modificacion VARCHAR(100),
-    @P_Fecha_Creacion DATETIME,
     @P_Fecha_Modificacion DATETIME
 AS
 BEGIN
@@ -413,6 +410,7 @@ BEGIN
         RETURN 0
     END CATCH
 END;
+
 GO
 
 /****** Object:  StoredProcedure [dbo].[sp_EliminarArticulos]    Script Date: 6/11/2024 8:56:30 AM ******/
