12ff1f9f5e12eaa1e5845b3fa048c56f9351b1f2
Aaron Chaves <heidricrico@gmail.com>
2024-06-20 18:54:31 -0600

    Commit Crud Seguridad
    Commit Crud Seguridad

---
+++ ADM.API/Controllers/SeguridadController.cs
@@ -0,0 +1,135 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using ADM.API;
+using ADM.Models;
+using ADM.Service;
+using Microsoft.AspNetCore.Mvc;
+using ADM.Interface;
+using ADM.Architectur;
+using ADM.Architecture;
+using Newtonsoft.Json;
+
+namespace ADM.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeguridadController : Controller
+    {
+        private ISeguridadService? _SeguridadService;
+
+
+        [HttpGet]
+        [Route("Listar")]
+        public IEnumerable<Seguridad> ListaSeguridades(string? Usuario)
+        {
+            DataTable tSeguridad = null;
+
+            if (Usuario == null)
+            {
+                tSeguridad = DBData.List("sp_ListarSeguridad");
+            }
+            else
+            {
+                List<DBParameter> parameters = new List<DBParameter>
+                    {
+                        new DBParameter("@Usuario", Usuario)
+                    };
+                tSeguridad = DBData.List("sp_ListarSeguridadxUsuario", parameters);
+            }
+            string jsonArticle = JsonConvert.SerializeObject(tSeguridad);
+            var result = JsonProvider.DeserializeSimple<IEnumerable<Seguridad>>(jsonArticle);
+            return result;
+        }
+
+        [HttpGet]
+        [Route("Obtener/{PK}")]
+        public Seguridad ObtenerSeguridad(string? PK)
+        {
+            if (PK != null)
+            {
+                List<DBParameter> parameters = new List<DBParameter>
+                    {
+                        new DBParameter("@PK_IdUsuario", PK)
+                    };
+                DataTable tSeguridad = DBData.List("sp_ObtenerSeguridad", parameters);
+                string jsonArticle = JsonConvert.SerializeObject(tSeguridad);
+                var result = JsonProvider.DeserializeSimple<IEnumerable<Seguridad>>(jsonArticle);
+
+
+                return result.FirstOrDefault();
+            }
+            else
+            {
+                return new Seguridad();
+            }
+        }
+
+        [HttpPost]
+        [Route("Crear")]
+        public bool CrearSeguridad(Seguridad Seguridad)
+        {
+            List<DBParameter> parameters = new List<DBParameter>
+            {
+                new DBParameter("@P_PK_IdUsuario", Seguridad.PK_IdUsuario.ToString()),
+                new DBParameter("@P_Usuario", Seguridad.Usuario),
+                new DBParameter("@P_Correo", Seguridad.Correo),
+                new DBParameter("@P_Contra", Seguridad.Contra),
+                new DBParameter("@P_Rol", Seguridad.Rol.ToString()), //El rol en la bd es int
+                new DBParameter("@P_Estado", Seguridad.Estado.ToString()),
+                new DBParameter("@P_FK_Usuario_Creacion", Seguridad.FK_Usuario_Creacion),
+                new DBParameter("@P_FK_Usuario_Modificacion", Seguridad.FK_Usuario_Modificacion),
+                new DBParameter("@P_Fecha_Creacion", DateTime.Now.ToString("yyyy-MM-dd")),
+                new DBParameter("@P_Fecha_Modificacion", DateTime.Now.ToString("yyyy-MM-dd"))
+            };
+
+            var result = DBData.Execute("sp_InsertarSeguridad", parameters);
+
+            return result;
+        }
+
+        [HttpPost]
+        [Route("Actualizar")]
+        public bool ActualizarSeguridad(Seguridad Seguridad)
+        {
+            List<DBParameter> parameters = new List<DBParameter>
+            {
+                new DBParameter("@P_PK_IdUsuario", Seguridad.PK_IdUsuario.ToString()),
+                new DBParameter("@P_Usuario", Seguridad.Usuario),
+                new DBParameter("@P_Correo", Seguridad.Correo),
+                new DBParameter("@P_Contra", Seguridad.Contra),
+                new DBParameter("@P_Rol", Seguridad.Rol.ToString()), //El rol en la bd es int
+                new DBParameter("@P_Estado", Seguridad.Estado.ToString()),
+                new DBParameter("@P_FK_Usuario_Creacion", Seguridad.FK_Usuario_Creacion),
+                new DBParameter("@P_FK_Usuario_Modificacion", Seguridad.FK_Usuario_Modificacion),
+                new DBParameter("@P_Fecha_Creacion", DateTime.Now.ToString("yyyy-MM-dd")),
+                new DBParameter("@P_Fecha_Modificacion", DateTime.Now.ToString("yyyy-MM-dd"))
+            };
+
+            var result = DBData.Execute("sp_ModificarSeguridad", parameters);
+
+            return result;
+        }
+
+        [HttpPost]
+        [Route("Desactivar/{PK}")]
+        public bool DesactivarSeguridad(string PK)
+        {
+            if (PK == null || PK.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                List<DBParameter> parameters = new List<DBParameter>
+                {
+                    new DBParameter("@P_PK_IdUsuario", PK)
+                };
+
+                var result = DBData.Execute("sp_EliminarSeguridad", parameters);
+
+                return result;
+            }
+        }
+    }
+}
--- ADMFacturar/Controllers/SeguridadController.cs
+++ ADMFacturar/Controllers/SeguridadController.cs
@@ -1,30 +1,132 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ADM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace ADMFacturar.Controllers
 {
     public class SeguridadController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        public SeguridadController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7270/api");
         }
-        public IActionResult Login()
-        {
-            return View();
-        }
-        public IActionResult CreateUser()
-        {
-            return View();
 
-        }
-        public IActionResult UpdateUser()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            {
+                var resp = await _httpClient.GetAsync("api/Seguridad/Listar");
 
+                if (resp.IsSuccessStatusCode)
+                {
+                    var content = await resp.Content.ReadAsStringAsync(); //Lee la respuesta del API
+                    var seguridades = JsonConvert.DeserializeObject<IEnumerable<Seguridad>>(content);
+                    return View("Index", seguridades);
+                }
+
+                return View(new List<Seguridad>());
+            }
         }
-        public IActionResult List()
+
+        public IActionResult CrearSeguridad()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearSeguridad(Seguridad seguridad)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(seguridad);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+                var response = await _httpClient.PostAsync("/api/Seguridad/Crear", content);
+
+                // Log the response content for debugging
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response from API: " + responseContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            return View(seguridad);
+        }
+
+        public async Task<IActionResult> ActualizarSeguridad(int? PK)
+        {
+            var resp = await _httpClient.GetAsync($"api/Seguridad/Obtener/{PK}");
+
+            if (resp.IsSuccessStatusCode)
+            {
+                var content = await resp.Content.ReadAsStringAsync(); //Lee la respuesta del API
+                var seguridad = JsonConvert.DeserializeObject<Seguridad>(content);
+                return View("ActualizarSeguridad", seguridad); // Devuelve 'seguridad' en lugar de 'resp'
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ActualizarSeguridad(Seguridad seguridad)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(seguridad);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+                var response = await _httpClient.PostAsync("/api/Seguridad/Actualizar", content);
+
+                // Log the response content for debugging
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response from API: " + responseContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            return View(seguridad);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DesactivarSeguridad(string PK)
+        {
+            if (ModelState.IsValid)
+            {
+                var content = new StringContent(PK, Encoding.UTF8, "application/json");
+                var resp = await _httpClient.PostAsync($"/api/Seguridad/Desactivar/{PK}", content);
+                string responseContent = await resp.Content.ReadAsStringAsync();
+                Console.WriteLine("Response from API: " + responseContent);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index"); // Devuelve 'Seguridad' en lugar de 'resp'
+                }
+
+                return NotFound();
+
+            }
+            return Ok();
         }
     }
 }
