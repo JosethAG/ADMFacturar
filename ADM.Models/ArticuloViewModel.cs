8ef52e1d282ea275bb7c1f8cf06f7054b1b595da
ADelgado-99 <74467915+ADelgado-99@users.noreply.github.com>
2024-06-23 13:23:03 -0600

    Insert completo Ingreso

--- ADM.Models/ArticuloViewModel.cs
+++ ADM.Models/ArticuloViewModel.cs
@@ -4,11 +4,11 @@
     {
         public string PK_Articulo { get; set; }
         public string Descripcion { get; set; }
-        public string Codigo_Barras { get; set; }
-        public string FK_Proveedor { get; set; }
+        public string? Codigo_Barras { get; set; }
+        public string? FK_Proveedor { get; set; }
         public int Cantidad { get; set; }
         public double Costo { get; set; }
         public double Precio { get; set; }
-        public bool Estado { get; set; }
+        public bool? Estado { get; set; }
     }
 }
