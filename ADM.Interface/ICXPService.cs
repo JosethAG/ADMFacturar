c61ac7f4d2f09fcd5ed33cec06b2452f14239fa0
Ronald2217 <ronald2217@gmail.com>
2024-07-08 20:44:59 -0600

    Commit controladores y vistas CXC y CXP

---
+++ ADM.Interface/ICXPService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADM.Models;
+
+namespace ADM.Interface
+{
+    public interface ICXPService
+    {
+        IEnumerable<CXP> ListaCXP(string? Nombre);
+        bool CrearUpdCXP(CXP cxp);
+        bool EliminarCXP(string Id);
+
+
+    }
+}
---
+++ ADM.Models/AbonoCXP.cs
@@ -0,0 +1,21 @@
+﻿namespace ADM.Models
+{
+    public class AbonoCXP
+    {
+        public string Numero_Recibo { get; set; }
+        public string FK_Documento { get; set; }
+        public string FK_Proveedor { get; set; }
+        public DateTime Fecha_Documento { get; set; }
+        public decimal Monto { get; set; }
+        public decimal Saldo_Pendiente { get; set; }
+        public decimal Monto_Abonado { get; set; }
+        public string Tipo_Documento { get; set; }
+        public string Banco { get; set; }
+        public string? FK_Usuario_Creacion { get; set; }
+        public string? FK_Usuario_Modificacion { get; set; }
+        public DateTime Fecha_Creacion { get; set; }
+        public DateTime Fecha_Modificacion { get; set; }
+
+
+    }
+}
