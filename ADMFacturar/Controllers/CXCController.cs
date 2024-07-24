4db113929d8b38314f3b8687e73d27921116c2fd
Ronald2217 <ronald2217@gmail.com>
2024-07-23 19:01:15 -0600

    Cambio de dolar a colones

--- ADMFacturar/Controllers/CXCController.cs
+++ ADMFacturar/Controllers/CXCController.cs
@@ -69,8 +69,7 @@ namespace ADMFacturar.Controllers
             {
                 var content = await resp.Content.ReadAsStringAsync();
                 var abonos = JsonConvert.DeserializeObject<IEnumerable<AbonoCXC>>(content);
-                ViewData["AbonosCXC"] = abonos ?? new List<AbonoCXC>();
-                return View("Detalles");
+                return View("abonos");
             }
 
             return View();
