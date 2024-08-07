7ecb10a7d0b8f0de0423879627e65a4ab1b81f50
Ronald2217 <ronald2217@gmail.com>
2024-08-06 18:48:01 -0600

    Reporte Estados de cuentas

---
+++ ADM.Models/FacturaModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADM.Models
+{
+    public class FacturaModel
+    {
+        public int PK_Factura { get; set; }
+        public string Nombre_Cliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Fecha_Vencimiento { get; set; }
+        public string Estado { get; set; }
+        public decimal Total { get; set; }
+        public decimal Saldo_Pendiente { get; set; }
+    }
+}
